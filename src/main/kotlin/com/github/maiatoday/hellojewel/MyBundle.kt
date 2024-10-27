bb55d8a1023f06fbb3c994a060234e0d31f609ba
GitHub Action <action@github.com>
2024-10-27 18:14:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/maiatoday/hellojewel/MyBundle.kt
+++ src/main/kotlin/com/github/maiatoday/hellojewel/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.maiatoday.hellojewel
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/maiatoday/hellojewel/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/maiatoday/hellojewel/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.maiatoday.hellojewel.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
