8b5585a815eb9f2986a7d3fa5fea57e61f6cb154
Maia Grotepass <maiatoday@gmail.com>
2024-11-01 18:08:51 +0100

    Add a compose tab... that crashes

--- src/main/kotlin/com/github/maiatoday/hellojewel/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/maiatoday/hellojewel/toolWindow/MyToolWindowFactory.kt
@@ -10,19 +10,30 @@ import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
 import com.github.maiatoday.hellojewel.MyBundle
 import com.github.maiatoday.hellojewel.services.MyProjectService
+import com.github.maiatoday.hellojewel.ui.MyTestTab
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.util.NlsContexts.TabTitle
+import org.jetbrains.jewel.bridge.addComposeTab
 import javax.swing.JButton
+import javax.swing.JComponent
 
 
-class MyToolWindowFactory : ToolWindowFactory {
+class MyToolWindowFactory : ToolWindowFactory, DumbAware {
 
     init {
         thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
     }
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val myToolWindow = MyToolWindow(toolWindow)
-        val content = ContentFactory.getInstance().createContent(myToolWindow.getContent(), null, false)
-        toolWindow.contentManager.addContent(content)
+        toolWindow.addSwingTab(MyToolWindow(toolWindow).getContent(), "Hi")
+        toolWindow.addComposeTab("My Test Tab") { MyTestTab() }
+    }
+
+    private fun ToolWindow.addSwingTab(component: JComponent, @TabTitle title: String) {
+        val manager = contentManager
+        val tabContent = manager.factory.createContent(component, title, true)
+        tabContent.isCloseable = false
+        manager.addContent(tabContent)
     }
 
     override fun shouldBeAvailable(project: Project) = true
---
+++ src/main/kotlin/com/github/maiatoday/hellojewel/ui/MyTestTab.kt
@@ -0,0 +1,9 @@
+package com.github.maiatoday.hellojewel.ui
+
+import androidx.compose.runtime.Composable
+import org.jetbrains.jewel.ui.component.Text
+
+@Composable
+internal fun MyTestTab() {
+    Text("Hello World!")
+}
